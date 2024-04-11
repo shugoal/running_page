bbfc8999c4a7e452a3d4a878ee274b311eb108a5
shugoal <shugoal@163.com>
2024-04-11 14:53:44 +0800

    Add files via upload

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,29 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const data: ISiteMetadataResult = {
+  siteTitle: '桐宇的运动',
+  siteUrl: 'https://yihong.run',
+  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'Blog',
+      url: 'https://github.com/shugoal/gitblog',
+    },
+    {
+      name: 'About',
+      url: 'https://github.com/shugoal/running_page/blob/main/README-CN.md',
+    },
+  ],
+};
+
+export default data;
